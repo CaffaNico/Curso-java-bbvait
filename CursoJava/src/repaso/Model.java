5d387c6f157711e920ba49c96a2976796261d23c
Nico <nicolas.caffa@bbvaitspain.com>
2021-05-10 13:39:15 +0200

    Creacion interfaz y clase padre

---
+++ CursoJava/src/repaso/Model.java
@@ -0,0 +1,12 @@
+package repaso;
+/**
+ * Esta interfaz obliga a implemetar estos metodos
+ * @author Nicolas
+ *
+ */
+public interface Model {
+	public boolean equals(Object obj);
+	public int hashCode();
+	public String toString();
+
+}
