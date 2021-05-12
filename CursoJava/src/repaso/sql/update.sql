33a2178fb62f225862dfc1965f5a8a4ef52c9e2a
Nico <nicolas.caffa@bbvaitspain.com>
2021-05-12 12:54:37 +0200

    Archivo sql updates

---
+++ CursoJava/src/repaso/sql/update.sql
@@ -0,0 +1,5 @@
+UPDATE figuras SET fig_nombre = 'Cuadrado'  WHERE fig_id = 1
+UPDATE figuras SET fig_nombre = 'Poligono'  WHERE fig_id = 5
+UPDATE figuras SET fig_nombre = 'Circulo'  WHERE fig_id = 6
+UPDATE figuras SET fig_nombre = 'Rectangulo'  WHERE fig_id = 7
+UPDATE figuras SET fig_nombre = 'Triangulo'  WHERE fig_id = 8
